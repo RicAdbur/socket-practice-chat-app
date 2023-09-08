865dedffc0f2a087be4bccb70c23d133139b32cf
Max <maxrrice@hotmail.com>
2023-09-08 18:30:59 -0500

    fixed typo

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on('chat message', msg => {
-    io.emit('message: ' + msg);
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg);
     // console.log(msg)
   });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
